fc4bc6e28f50a88c39c2a2d2d070414cee6c3e82
Roialfassi <roialfassi@gmail.com>
2022-12-26 13:44:04 +0200

    Add Files

---
+++ tensorFlowLite-MNIST.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+# Load MNIST dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# Preprocess the data
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Build the model
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate the model
+_, acc = model.evaluate(x_test, y_test)
+print('Test accuracy:', acc)
